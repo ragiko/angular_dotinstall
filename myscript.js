d628a6189711230ef9ac4263de2e7bfb64e07741
Taguchi Hiroaki <ragiko.complexe@gmail.com>
2014-10-14 23:25:44 +0900

    #08 フォームの基本を学ぼう

--- myscript.js
+++ myscript.js
@@ -1,21 +1,6 @@
 var MainCtrl = function ($scope) {
-    $scope.users = [
-        {name:'taguchi', score:52.22},
-        {name:'tanaka', score:28.22},
-        {name:'tanaka', score:28.22},
-        {name:'tanaka', score:28.22},
-        {name:'tanaka', score:28.22},
-        {name:'tanaka', score:28.22},
-        {name:'tanaka', score:28.22},
-        {name:'yamada', score:11.11}]
+    $scope.adduser = function () {
 
-    $scope.today = new Date();
-}
-
-
-var userItemCtrl = function ($scope) {
-    $scope.increment = function () {
-        $scope.user.score++;
     }
-
 }
+
