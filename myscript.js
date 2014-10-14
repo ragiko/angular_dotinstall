2f9a632ec147aa2440b8d309e9b9457198a65f6a
Taguchi Hiroaki <ragiko.complexe@gmail.com>
2014-10-14 23:06:35 +0900

    #04 フィルターを使ってみよう

--- myscript.js
+++ myscript.js
@@ -3,4 +3,6 @@ var MainCtrl = function ($scope) {
         {name:'taguchi', score:52.22},
         {name:'tanaka', score:28.22},
         {name:'yamada', score:11.11}]
+
+    $scope.today = new Date();
 }
