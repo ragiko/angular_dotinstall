cb89efd29a718942097fb11c7879dea2cd7f3ec5
Taguchi Hiroaki <ragiko.complexe@gmail.com>
2014-10-14 23:10:33 +0900

    #05 ループで使えるフィルター(1)

--- myscript.js
+++ myscript.js
@@ -2,6 +2,11 @@ var MainCtrl = function ($scope) {
     $scope.users = [
         {name:'taguchi', score:52.22},
         {name:'tanaka', score:28.22},
+        {name:'tanaka', score:28.22},
+        {name:'tanaka', score:28.22},
+        {name:'tanaka', score:28.22},
+        {name:'tanaka', score:28.22},
+        {name:'tanaka', score:28.22},
         {name:'yamada', score:11.11}]
 
     $scope.today = new Date();
